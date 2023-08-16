fd0dc1759ecff571ef5925addb44ee86568dad9e
Purwa Aditya Wisnu Wardhana <adityawisnu104@gmail.com>
2023-08-16 15:34:04 +0700

    rollbck vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        // /^node:.*/,
-        "@react-hook/window-size",
-      ],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       // /^node:.*/,
+  //       "@react-hook/window-size",
+  //     ],
+  //   },
+  // },
 });
